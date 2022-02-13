9aa205a150bf1d7d2367e4405c02c6f7d9e06317
Francis Kinyuru <franciskinyuru26@gmail.com>
2022-02-13 14:48:50 +0300

    check if number is fibonacci or not code

---
+++ fibonacci.py
@@ -0,0 +1,22 @@
+import math
+
+# A utility function that will return true
+# if the number is perfect square, else this
+# will return false
+def isPerfectSquare(num):
+    # finding the square root of num
+    s = int(math.sqrt(num))
+    return s * s == num
+
+
+def isFibonacciNumber(n):
+    # return true if the number is fibonacci otherwise
+    # return false
+    return isPerfectSquare(5 * n * n + 4) or isPerfectSquare(5 * n * n - 4)
+
+
+n = int(input("Enter the number you want to check : "))
+if (isFibonacciNumber(n) == True):
+    print(n, " Given number is fibonacci number")
+else:
+    print(n, " is not a fibonacci number")
